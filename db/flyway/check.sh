c4e510831a52d61ca967b2bb6fa1823e1f5f6ac9
Hasan Masum <hasanmasum1852@gmail.com>
2024-12-12 16:53:49 +0600

    feat: enhance migration check script to validate new migration file versions

--- db/flyway/check.sh
+++ db/flyway/check.sh
@@ -1,12 +1,3 @@
-
-last_migration_version=$(git ls-tree origin/master -r --name-only | grep 'migrations' | grep '.[0-9]' -o | sort -r | head -n 1 | grep '[0-9]' -o)
-
-if [ -z "$last_migration_version" ]; then
-    echo "No old migrations file found."
-else
-    echo "Last migration file version: $last_migration_version"
-fi
-
 flyway_changes=$(git diff origin/master HEAD --name-only | grep 'db/flyway/migrations')
 
 if [ -z "$flyway_changes" ]; then
@@ -18,11 +9,48 @@ else
 
     # check if there a change is old migrations file
     if [ -z "$(echo "$changes" | grep '-')" ]; then
-        echo "No old migrations file changed."
+        echo "No old migrations file changed.\n"
     else
         echo "Old migrations file changed:"
         echo "$(echo "$changes" | grep '-')"
-        echo "Please do not modify old migrations files."
+        echo "Please do not modify old migrations files.\n"
         exit 1
+    fi
+
+    new_versions=$(echo "$flyway_changes" | grep '.[0-9]' -o)
+    # check if there is more than one new migration file
+    if [ "$(echo "$new_versions" | wc -l)" -gt 1 ]; then
+        echo "More than one new migration file found. Only one new migration file is allowed.\n"
+        exit 1
+    fi
+
+    new_version=$(echo "$flyway_changes" | grep '.[0-9]' -o | sort | head -n 1 | grep '[0-9]' -o)
+    last_migration_version=$(git ls-tree origin/master -r --name-only | grep 'migrations' | grep '.[0-9]' -o | sort -r | head -n 1 | grep '[0-9]' -o)
+
+    if [ -z "$last_migration_version" ]; then
+        echo "No old migrations file found.\n"
+        # new version should be 1
+        if [ "$new_version" -ne 1 ]; then
+            echo "New migration file version should be 1.\n"
+            exit 1
+        fi
+    else
+        echo "Last migration file version: $last_migration_version\n"
+    fi
+
+    if [ -z "$new_version" ]; then
+        echo "No new migration file found.\n"
+    else
+        echo "New migration file version: $new_version\n"
+        if [ "$new_version" -le "$last_migration_version" ]; then
+            echo "New migration file version is less than or equal to the last migration file version.\n"
+            exit 1
+        fi
+
+        # difference between new and last migration file version should be 1
+        if [ "$((new_version - last_migration_version))" -ne 1 ]; then
+            echo "Difference between new and last migration file version should be 1.\n"
+            exit 1
+        fi
     fi
 fi
