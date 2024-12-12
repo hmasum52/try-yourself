111592242295f304a45efc3bfe4177fcd79f04a7
Hasan Masum <hasanmasum1852@gmail.com>
2024-12-12 16:41:34 +0600

    feat: add last migration version check and update person name in migration script

--- db/flyway/check.sh
+++ db/flyway/check.sh
@@ -1,13 +1,20 @@
+
+last_migration_version=$(git ls-tree origin/master -r --name-only | grep 'migrations' | grep '.[0-9]' -o | sort -r | head -n 1 | grep '[0-9]' -o)
+
+if [ -z "$last_migration_version" ]; then
+    echo "No old migrations file found."
+else
+    echo "Last migration file version: $last_migration_version"
+fi
+
 flyway_changes=$(git diff origin/master HEAD --name-only | grep 'db/flyway/migrations')
+
 if [ -z "$flyway_changes" ]; then
     echo "No Flyway migration files changed."
 else
     echo "Flyway migration files changed:"
     echo "$flyway_changes \n"
     changes=$(git diff origin/master HEAD --compact-summary | grep 'db/flyway/migrations')
-
-    old_migrations=$(git ls-tree origin/master -r --name-only | grep 'migrations' | grep 'V[0-9]')
-    echo $old_migrations
 
     # check if there a change is old migrations file
     if [ -z "$(echo "$changes" | grep '-')" ]; then
@@ -18,6 +25,4 @@ else
         echo "Please do not modify old migrations files."
         exit 1
     fi
-
-    
 fi
